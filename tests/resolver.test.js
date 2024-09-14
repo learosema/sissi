fa1e447876f05aced457bff8e067072d4bf8123a
Lea Rosema <lea@lea.lgbt>
2024-09-14 12:22:20 +0000

    fix: add basic test for resolver

---
+++ tests/resolver.test.js
@@ -0,0 +1,29 @@
+import { describe, it, mock, beforeEach, afterEach } from 'node:test';
+import assert from 'node:assert/strict';
+
+import { SissiConfig } from '../src/sissi-config.js';
+import { defaultResolver } from '../src/resolver.js';
+
+describe('defaultResolver', () => {
+
+  let config = new SissiConfig({
+    input: 'demo',
+    output: 'dist'
+  });
+
+  it('should return a function', () => {
+    const resolver = defaultResolver(config);
+
+    assert.equal(typeof resolver, 'function');
+  });
+
+  it('should resolve files from the local file system', async () => {
+    const resolve = defaultResolver(config);
+
+    const content = await resolve('index.html');
+    assert(content.startsWith('<!DOCTYPE html>');
+  });
+
+
+
+});
