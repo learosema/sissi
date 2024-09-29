106af4fb3615dd506327953b1268302acfe326eb
Lea Rosema <lea@lea.lgbt>
2024-09-30 01:39:18 +0200

    feat: add filters

--- tests/transforms/template-data.test.js
+++ tests/transforms/template-data.test.js
@@ -2,7 +2,7 @@ import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 import path from 'node:path';
 
-import { dataPath, handleTemplateFile, template } from '../../src/transforms/template-data.js';
+import { dataPath, handleTemplateFile, parseArguments, template } from '../../src/transforms/template-data.js';
 import { SissiConfig } from '../../src/sissi-config.js';
 import md from '../../src/md.js';
 
@@ -13,6 +13,12 @@ const TEST_DATA = {
     'authors': ['Joe', 'Lea'],
   },
   'theMatrix': [[1,2,3],[4,5,6],[7,8,9]],
+  date() {
+    return '12.03.2024'
+  },
+  greet(str) {
+    return 'Hello ' + str;
+  }
 }
 
 const TEST_MD = `---
@@ -31,6 +37,12 @@ const TEST_TEMPLATE = `<h1>{{ title }}</h1>
 const TEST_TEMPLATE_EXPECTED = `<h1>This is a title</h1>
 <p>Blog article by Lea</p>
 `
+
+const TEST_TEMPLATE_2 = `{{ date }}`
+const TEST_TEMPLATE_EXPECTED_2 = '12.03.2024';
+
+const TEST_TEMPLATE_3 = `{{ greet(meta.authors[1]) }}`
+const TEST_TEMPLATE_EXPECTED_3 = 'Hello Lea';
 
 describe('dataPath tests', () => {
 
@@ -57,6 +69,17 @@ describe('dataPath tests', () => {
     assert.throws(() => dataPath('object.[1]'));
     assert.throws(() => dataPath('object..[1]'));
   });
+});
+
+describe('parseArguments function', () => {
+  it('should parse argument lists', () => {
+    assert.deepEqual(parseArguments('"DE"', {}), ["DE"]);
+    assert.deepEqual(parseArguments('12.3, "DE"', {}), [12.3, "DE"]);
+  });
+
+  it('should support data path arguments', () => {
+    assert.deepEqual(parseArguments('meta.author, 12', {meta:{author:'Lea'}}), ['Lea', 12]);
+  });
 
 });
 
@@ -64,7 +87,20 @@ describe('template function', () => {
   it('should insert data into the placeholders wrapped in double curly brackets', () => {
     assert.equal(template(TEST_TEMPLATE)(TEST_DATA), TEST_TEMPLATE_EXPECTED);
   });
-})
+
+  it('should be able to invoke functions', () => {
+    assert.equal(template(TEST_TEMPLATE_2)(TEST_DATA), TEST_TEMPLATE_EXPECTED_2);
+    assert.equal(template(TEST_TEMPLATE_3)(TEST_DATA), TEST_TEMPLATE_EXPECTED_3);
+  });
+
+  it('should be able to apply a filter', () => {
+    const filters = new Map();
+    filters.set('shout', (str) => (str||'').toUpperCase());
+    const result = template('{{greeting | shout }}')({greeting: "Hello"}, filters);
+    
+    assert.equal(result, "HELLO");
+  })
+});
 
 describe('handleTemplateFile function', () => {
   it('should work with the default markdown plugin', async () => {
