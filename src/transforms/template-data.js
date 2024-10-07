f156cc6c1a486ab5ad5596ac287c1006702e9377
Lea Rosema <lea@lea.lgbt>
2024-10-07 23:14:04 +0200

    feat: rework template engine so it supports JS expressions

--- src/transforms/template-data.js
+++ src/transforms/template-data.js
@@ -1,12 +1,10 @@
 import path from 'node:path';
-
+import vm from 'node:vm';
 import { frontmatter } from './frontmatter.js';
 import { resolve } from '../resolver.js';
 import { SissiConfig } from "../sissi-config.js";
 
-const TEMPLATE_REGEX = /\{\{\s*([\w\.\[\]]+)(?:\((.*)\))?(?:\s*\|\s([a-zA-Z*]\w*)?(?:\s*\:\s*(.+))?)?\s*\}\}/g;
-const JSON_PATH_REGEX = /^[a-zA-Z_]\w*((?:\.\w+)|(?:\[\d+\]))*$/
-const JSON_PATH_TOKEN = /(^[a-zA-Z_]\w*)|(\.[a-zA-Z_]\w*)|(\[\d+\])/g
+const TEMPLATE_REGEX = /\{\{\s*(.+?)\s*\}\}/g;
 
 function mergeMaps(map1, map2) {
   return new Map([...map1, ...map2]);
@@ -16,49 +14,33 @@ function htmlEscape(input) {
   return input?.toString().replace(/\&/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
 }
 
-/**
- * Poor girl's jsonpath
- * 
- * @param {string} path 
- * @returns {(data:any) => any} a function that returns the specified property
- */
-export function dataPath(path) {
-  if (JSON_PATH_REGEX.test(path) === false) {
-    throw new Error('invalid json path: ' + path);
-  }
-  const matches = Array.from(path.match(JSON_PATH_TOKEN)).map(
-    m => JSON.parse(m.replace(/(^\.)|\[|\]/g, '').replace(/^([a-zA-Z]\w+)$/, '"$1"'))
-  );
-  return (data) => {
-    let result = data;
-    for (const match of matches) {
-      result = result[match];
-      if (! result) {
-        return result;
-      }
-    }
+function safeEval(snippet, context) {
+  try {
+    const s = new vm.Script(snippet);
+    let result = context && vm.isContext(context) ? s.runInContext(context) : s.runInNewContext(context || {Object, Array});
     return result;
+  } catch (err) {
+    if (err.name === 'ReferenceError') {
+      console.warn(`ReferenceError: ${err.message}`);
+      return '';
+    }
+    throw err;
   }
 }
-/**
- * 
- * @param {string} args a string with a comma separated  
- * @param {any} data data object that is used to fill in data-path parameters
- * @returns 
- */
-export function parseArguments(args, data) {
-  if (!args) return [];
-  return args.trim().split(/\s*,\s*/).map(arg => {
-    if (JSON_PATH_REGEX.test(arg)) {
-      return dataPath(arg)(data);
-    }
-    try {
-      return JSON.parse(arg)
-    } catch (err) {
-      console.error('error parsing JSON:', err.message);
-      return [];
-    }
-  });
+
+export function parseFilterExpression(expr, ctx) {
+  const colonSyntax = expr.match(/^([a-zA-Z_]\w+?)(?:\: (.+?))?$/);
+  if (colonSyntax !== null) {
+    const filter = colonSyntax[1];
+    const args = colonSyntax[2] ? Array.from(safeEval(`[${colonSyntax[2]}]`, ctx)).map(item => {
+      if (typeof item === 'function') {
+        return item();
+      }
+      return item;
+    }) : null;
+    return [filter, args];
+  }
+  throw new Error('filter syntax error');
 }
 
 /**
@@ -72,20 +54,30 @@ export function template(str) {
   let isSafe = false;
   defaultFilters.set('safe', (input) => { isSafe = true; return input; })
   return (data, providedFilters) => {
+    const context = vm.createContext({...data});
     const filters = mergeMaps(defaultFilters || new Map(), providedFilters || new Map())
-    return str.replace(TEMPLATE_REGEX, (_, expr, params, filter, filterParams) => {
-      let result = dataPath(expr)(data);
-      const args = parseArguments(params, data);
+    return str.replace(TEMPLATE_REGEX, (_, templateString) => {
+      const expressions = templateString.split('|').map(e => e.trim());
+      const mainExpression = expressions[0];
+      const filterExpressions = expressions.slice(1);
+      let result = safeEval(mainExpression, context);
+      if (typeof result === 'undefined') {
+        result = '';
+      }
+      if (typeof result === 'function') {
+        result = result();
+      }
+      for (const filterExpression of filterExpressions) {
+        const [filter, args] = parseFilterExpression(filterExpression, context);
+        if (!filter || filters instanceof Map === false || !filters.has(filter) ||
+          typeof filters.get(filter) !== 'function') {
+          // TODO: more helpful error message:
+          throw new Error('unregistered or invalid filter: ' + filter);
+        }
+        
+        result = args ? filters.get(filter)(result, ...args) : filters.get(filter)(result);
+      }
       
-      if (typeof result === "function") {
-        result = result(...args);
-      }
-
-      if (filter && filters instanceof Map &&
-          filters.has(filter) && typeof filters.get(filter) === 'function') {
-        const filterArgs = parseArguments(filterParams, data);
-        result = filters.get(filter)(result, ...(filterArgs||[]));
-      }
       return isSafe ? result : htmlEscape(result);
     });
   }
