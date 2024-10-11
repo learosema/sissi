d1be066899c2568dda70e8f1ddef72229f98329d
Lea Rosema <lea@lea.lgbt>
2024-10-11 18:10:34 +0200

    feat: support import layer syntax in css

--- src/css.js
+++ src/css.js
@@ -2,8 +2,7 @@ import path from 'path';
 
 import { handleTemplateFile } from './transforms/template-data.js';
 
-// TODO: add a regex for layer syntax
-const INCLUDE_REGEX = /@import [\"\']([\w:\/\\]+\.css)[\"\'];/g;
+const INCLUDE_REGEX = /@import [\"\']([\w:\/\\\.\-]+?\.css)[\"\'](?:\s*layer\s*\((\w+)\));/g;
 
 export default (config) => {
   config.addTemplateFormats('css');
@@ -16,8 +15,12 @@ export default (config) => {
       return async (data) => {
         const includes = new Map();
         const matches = inputContent.matchAll(INCLUDE_REGEX);
-        for (const [, file] of matches) {
+        for (const [, file, layer] of matches) {
           const tpl = await handleTemplateFile(config, data, path.join(parsed.dir, file));
+          if (layer) {
+            includes.set(file, tpl ? `@layer ${layer} {\n${tpl.content}\n}` : `@import url("${file}") layer(${layer});`);
+            continue;
+          }
           includes.set(file, tpl ? tpl.content : `@import url("${file}");`);
         }
         return inputContent.replace(INCLUDE_REGEX, (_, file) => includes.get(file)) 
