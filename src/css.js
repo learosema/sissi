0309cea44a03a4c2d8e9d9c072f5fc3ee8db2597
Lea Rosema <lea@lea.lgbt>
2024-10-18 23:11:37 +0200

    refactor: remove duplicated code, unify includes.

--- src/css.js
+++ src/css.js
@@ -1,6 +1,4 @@
-import path from 'path';
-
-import { handleTemplateFile } from './transforms/template-data.js';
+import { handleIncludes } from './includes.js';
 
 const INCLUDE_REGEX = /@import [\"\']([\w:\/\\\.\-]+?\.css)[\"\'](?:\s*layer\s*\((\w+)\))?;/g;
 
@@ -10,21 +8,22 @@ export default (config) => {
   config.addExtension('css', {
     outputFileExtension: 'css',
     compile: async function (inputContent, inputPath) {
-      let parsed = path.parse(inputPath);
-
       return async (data) => {
-        const includes = new Map();
-        const matches = inputContent.matchAll(INCLUDE_REGEX);
-        for (const [, file, layer] of matches) {
-          const tpl = await handleTemplateFile(config, data, path.join(parsed.dir, file));
-          if (layer) {
-            includes.set(file, tpl ? `@layer ${layer} {\n${tpl.content}\n}` : `@import url("${file}") layer(${layer});`);
-            continue;
+        return await handleIncludes(
+          inputContent, inputPath,
+          config, data, null, INCLUDE_REGEX, 
+          (context, _, file, layer) => {
+            const include = context.includes.get(file);
+            if (!include || !include.content) {
+              return `@import url("${file}")${layer ?` layer(${layer});`:``};`;
+            }
+            if (! layer) {
+              return include.content;
+            }
+            return `@layer ${layer} {\n${tpl.content}\n}`;
           }
-          includes.set(file, tpl ? tpl.content : `@import url("${file}");`);
-        }
-        return inputContent.replace(INCLUDE_REGEX, (_, file) => includes.get(file)) 
-      };
+        );
+      }
     },
   });
 };
--- src/html.js
+++ src/html.js
@@ -1,5 +1,4 @@
-import path from 'path';
-import { handleTemplateFile } from './transforms/template-data.js';
+import { handleIncludes } from './includes.js';
 
 const INCLUDE_REGEX = /<html-include[\s\r\n]*src="([\w\-\.]+?)"[\s\r\n]*\/?>/g;
 
@@ -9,24 +8,12 @@ export default (config) => {
   config.addExtension('html', {
     outputFileExtension: 'html',
     compile: async function (inputContent, inputPath) {
-      
-      let parsed = path.parse(inputPath);
-      
       return async (data) => {
-        const includes = new Map();
-        let content = inputContent;
-        const matches = Array.from(content.matchAll(INCLUDE_REGEX));
-        if (matches.length > 0) {
-          for (const [, file] of matches) {
-            const include = await handleTemplateFile(config, data, path.join(config.dir.includes, file));
-            includes.set(file, include ? include.content : `<!-- html-include src="${file}" -->`);
-          }
-          content = content.replace(INCLUDE_REGEX, (_, file) => {
-            return includes.get(file)
-          });
-        }
-        return content;
-      };
+        return await handleIncludes(
+          inputContent, inputPath, 
+          config, data, config.dir.includes, 
+          INCLUDE_REGEX);
+      }
     },
   });
 };
